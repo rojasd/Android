039d7240edbf9d93139e0a08016231bbd0e398f7
rojasd <donacianorojas@gmail.com>
2014-01-24 16:13:37 -0600

    zxczxc

--- OneMoreTime/OneMoreTime/Program.cs
+++ OneMoreTime/OneMoreTime/Program.cs
@@ -14,6 +14,7 @@ namespace OneMoreTime
             // Added after repote repo change
             // change
             // change from remote repository, more  , mas
+            // local change
         }
     }
 }
